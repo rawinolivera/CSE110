cef5e6f5381ec1987717347f88b800de64e4101c
rawinolivera <oli18001@byui.edu>
2022-11-03 21:00:38 -0500

    prove advanced

---
+++ prove02.py
@@ -0,0 +1,16 @@
+print('Please enter the following: ')
+print()
+adjective = input('adjective')
+animal = input('animal')
+verb1 = input('verb')
+exclamation = input('exclamation')
+verb2 = input('verb')
+verb3 = input('verb')
+print()
+print('Your story is: ') 
+print()
+print(f'The other day, I was really in trouble. It all started when I saw a very
+{adjective} {animal} {verb} down the hallway. "{exclamation.upper()}" I yelled. But all
+I could think to do was to {verb} over and over. Miraculously,
+that caused it to stop, but not before it tried to {verb}
+right in front of my family. ')
