92e7173529a138f6a8d240089360539ca36d06cc
rawinolivera <oli18001@byui.edu>
2022-11-04 00:39:19 -0500

    last activities done L02

--- prove02.py
+++ prove02.py
@@ -1,16 +1,16 @@
 print('Please enter the following: ')
 print()
-adjective = input('adjective')
-animal = input('animal')
-verb1 = input('verb')
-exclamation = input('exclamation')
-verb2 = input('verb')
-verb3 = input('verb')
+adjective = input('adjective: ')
+animal = input('animal: ')
+verb1 = input('verb(ing): ')
+exclamation = input('exclamation: ')
+verb2 = input('verb: ')
+verb3 = input('verb: ')
 print()
 print('Your story is: ') 
 print()
-print(f'The other day, I was really in trouble. It all started when I saw a very
-{adjective} {animal} {verb} down the hallway. "{exclamation.upper()}" I yelled. But all
-I could think to do was to {verb} over and over. Miraculously,
-that caused it to stop, but not before it tried to {verb}
-right in front of my family. ')
+print(f'The other day, I was really in trouble. It all started when I saw a very')
+print(f'{adjective.lower()} {animal.lower()} {verb1.lower()} down the hallway. "{exclamation.upper()}!" I yelled. But all')
+print(f'I could think to do was to {verb2.lower()} over and over. Miraculously,')
+print(f'that caused it to stop, but not before it tried to {verb3.lower()}')
+print(f'right in front of my family. ')
