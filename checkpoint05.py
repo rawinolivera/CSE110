8e14dfd851927af35c0c2ac5580ef36a56d3d114
rawinolivera <oli18001@byui.edu>
2022-11-30 19:18:44 -0500

    new

---
+++ checkpoint05.py
@@ -0,0 +1,24 @@
+first_num = int(input("What is the first number? "))
+second_num = int(input("What is the second number? "))
+
+if first_num > second_num:
+    print("The first number is greater")
+else:
+    print("The first number is not greater")
+if first_num == second_num:
+    print("The numbers are equal")
+else :
+    print("The numbers are not equal")
+if first_num < second_num:
+    print("The second number is greater")
+else:
+        print("The second number is not greater")
+
+
+print()
+
+animal = input("What is your favorite animal? ")
+if animal.lower() == 'dog':
+    print("That's my favorite animal too!")
+else:
+    print("That one is not my favorite.")
---
+++ team_activity_L05.py
@@ -0,0 +1,49 @@
+# grade = float(input("What is you grade percentage? "))
+
+# if grade > 70:
+#     print("Great Job, You passed the class")
+# else:
+#     print("Work hard the next time!")
+
+# if grade > 90:
+#     print("You letter grade is: A")
+# elif grade >= 80:
+#     print("You letter grade is: B")
+# elif grade >= 70:
+#     print("You letter grade is: C")
+# elif grade >= 60:
+#     print("You letter grade is: D")
+# else:
+#     print("You letter grade is: F")
+
+grade = float(input("What is you grade percentage? "))
+
+if grade > 70:
+    print("Great Job, You passed the class")
+else:
+    print("Work hard the next time!")
+
+if grade >= 93:
+    print("You letter grade is: A")
+elif grade < 93:
+    print("You letter grade is: A-")
+elif grade >= 87:
+    print("You letter grade is: B+")
+elif grade >= 83:
+    print("You letter grade is: B")
+elif grade < 83:
+    print("You letter grade is: B-")
+elif grade >= 77:
+    print("You letter grade is: C+")
+elif grade >= 73:
+    print("You letter grade is: C")
+elif grade < 73:
+    print("You letter grade is: C-")
+elif grade >= 67:
+    print("You letter grade is: D+")
+elif grade >= 63:
+    print("You letter grade is: D")
+elif grade < 63:
+    print("You letter grade is: D-")
+else:
+    print("You letter grade is: F")
